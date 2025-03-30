2aaf9d8626addebc1f2e871968d971dbf54ca326
Mengfei-Li-0507 <mfl0507@bu.edu>
2025-03-30 15:10:40 -0400

    Mar.30

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,27 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const myName = "Mengfei";
+const myAge = 20;
+
+console.log(myAge);
+
+// Practical JavaScript
+
+const toggledParagraph = document.querySelector('.show');
+console.log(toggledParagraph);
+
+// Write a Function that toggles the show/hide classes
+function toggleParagraph() {
+	toggledParagraph.classList.toggle("hide");
+	toggledParagraph.classList.toggle("show");
+	console.log("My toggle function")
+	console.log(toggledParagraph);
+}
+
+//Find the button and toggle the paragraph when we click it
+const toggleButton = document.querySelector("#toggle-control");
+console.log(toggleButton);
+
+toggleButton.addEventListener("click", toggleParagraph);
